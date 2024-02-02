2b99bca11ad43ae4d53501d8711c1a4b0ee25e4f
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-02-02 01:29:10 -0600

    Se agrego validacion de datos en el login

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,36 @@
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
 }
 
 
 inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
 });
+function validateLogin() {
+    const email = document.querySelector(".input-div.one .input").value;
+    const password = document.querySelector(".input-div.pass .input").value;
+    const users = [
+        { email: 'zmenendez@kinal.com', password: '28102006' },
+        { email: 'braulio@kinal.com', password: '1234567' },
+    ];
+
+    const matchingUser = users.find((user) => user.email === email && user.password === password);
+
+    if (matchingUser) {
+        window.location.href = "assets/pages/Contacto.html";
+    } else {
+        alert("Invalid email or password");
+    }
+}
